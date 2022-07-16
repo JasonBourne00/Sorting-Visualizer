a537a86b0672e08aa9d81a1cf33b05418627b536
Branislav Đurić <90508013+JasonBourne00@users.noreply.github.com>
2022-07-16 15:54:58 +0200

    Update SortingVisualizer.jsx
    Changed GUI with slider for delay animations and array size. 
    Insertion sort added.

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import './SortingVisualizer.css';
 import { mergeSortAnimation } from "../sortingAlg/sortingAlg";
+import { insertionSortAlgorithm } from "../sortingAlg/insertionalgorithm";
+
+const PRIMARY_COLOUR = "#069A8E";
+const SECONDARY_COLOUR = "red";
+
+
 export default class SortingVisualizer extends React.Component{
     constructor(props){
         super(props);
@@ -15,14 +21,14 @@ export default class SortingVisualizer extends React.Component{
     }
     resetArray(){
         const array=[];
-        let i=0;
-        while(i<310){
-          array.push(randomIntFromInterval(5,730));
-            i+=1;
+        let NUMBER_OF_ARRAY = document.getElementById("ARRAY_SIZE").value;
+        for (let i = 0; i < NUMBER_OF_ARRAY; i++) {
+          array.push(randomIntFromInterval(5,450));
         }
         this.setState({array});
     }
     mergeSort(){
+    let ANIMATION_SPEED = document.getElementById("DELAY").value; //animation technique starting
      const animations=mergeSortAnimation(this.state.array);
      for(let i=0; i<animations.length;i++){   
         const arrayBars=document.getElementsByClassName('array-bar');
@@ -31,17 +37,17 @@ export default class SortingVisualizer extends React.Component{
             const[barOneIdx,barTwoIdx]=animations[i];
             const barOneStyle=arrayBars[barOneIdx].style;
             const barTwoStyle=arrayBars[barTwoIdx].style;
-            const color = i % 3 === 0 ? 'red' : 'turquoise';
+            const color = i % 3 === 0 ? SECONDARY_COLOUR : PRIMARY_COLOUR;
             setTimeout(()=>{
                 barOneStyle.backgroundColor=color;
                 barTwoStyle.backgroundColor=color;
-            },i*5);
+            },i*ANIMATION_SPEED);
         }else{
             setTimeout(()=>{
                 const[barOneIdx,newHeight]=animations[i];
                 const barOneStyle = arrayBars[barOneIdx].style;
                 barOneStyle.height=`${newHeight}px`;
-            },i*5)
+            },i*ANIMATION_SPEED)
         }
     }
     
@@ -52,28 +58,52 @@ export default class SortingVisualizer extends React.Component{
     bubbleSort(){
 
     }
-    heapSort(){
-
+    insertionSort(){
+        let ANIMATION_SPEED = document.getElementById("DELAY").value;                 
+        var blocks = document.querySelectorAll(".array-bar"); 
+        const n = this.state.array.length;
+        insertionSortAlgorithm(this.state.array,n,blocks,ANIMATION_SPEED);
+          
     }
     render(){
-    const {array}=this.state;
-    
-    return(
+    const { array } = this.state;
+    return (
+      <div>
         <div className="array-container">
-        {array.map((value,idx) => (
-            <div className="array-bar" key={idx}
-            style={{height: `${value}px`}}>
-             </div>
-        ))}
-        <button color="primary" onClick={()=>this.resetArray()}>
-            Generate New Array
-        </button>
-        <button onClick={()=>this.quickSort()}>Quick sort</button>
-        <button onClick={()=>this.mergeSort()}>Merge sort</button>
-        <button onClick={()=>this.bubbleSort()}>Bubble sort</button>
-        <button onClick={()=>this.heapSort()}>Heap sort</button>
+          {array.map(
+            (
+              value,
+              ids //mapping bars
+            ) => (
+              <div
+                className="array-bar"
+                key={ids}
+                style={{
+                  backgroundColor: PRIMARY_COLOUR,
+                  height: `${value}px`,
+                }}
+              ></div>
+            )
+          )}
+        </div>
 
-    </div>
+        <div className="footer">
+          <label for="array-size">Size</label>
+          <input
+            id="ARRAY_SIZE"
+            name="array-size"
+            type="range"
+            min="5"
+            max="99"
+          ></input>
+          <label for="delay">Delay</label>
+          <input id="DELAY" name="delay" type="range" min="3" max="100"></input>
+          <button onClick={() => this.resetArray()}>Generate Array</button>
+          <button onClick={() => this.mergeSort()}>Merge Sort</button>
+          <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+          <button onClick={() => this.insertionSort()}>Insertion Sort</button>
+        </div>
+      </div>
     );
   }
 }
